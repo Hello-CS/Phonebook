2587c743d697fbbee134a2d5b4d30dbaa8f8b018
Hello-CS <mrlamchensian@gmail.com>
2021-01-12 01:52:56 +0800

    Edit

--- models/person.js
+++ models/person.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 
-console.log('connecting to', url);
+console.log('connecting to', process.env.MONGODB_URI);
 
 mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true})
 .then(result => {
