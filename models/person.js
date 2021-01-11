7451b026a6a00938079516131d68b2e385d01d9b
Hello-CS <mrlamchensian@gmail.com>
2021-01-12 00:04:02 +0800

    Edits

--- models/person.js
+++ models/person.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const url = process.env.MONGODB_URI
+const url = 'mongodb+srv://CS:sasuke@phonebook.liwbb.mongodb.net/phonebook?retryWrites=true&w=majority'
+//process.env.MONGODB_URI
 
 console.log('connecting to', url);
 
