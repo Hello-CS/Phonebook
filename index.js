195a74b165a3a737e3cd218675a06aafec9617d0
Hello-CS <mrlamchensian@gmail.com>
2021-01-12 01:22:29 +0800

    Edit

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ app.post('/api/persons', (req,res) => {
     });
 });
 
-const PORT = 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 });
